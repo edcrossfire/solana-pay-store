eafa1fe7a664fe78395b65817d7f78ebf1232cdd
edcrossfire <ed@crossfire.network>
2022-07-01 19:28:15 -0400

    Finished loyalty program

--- lib/addresses.ts
+++ lib/addresses.ts
@@ -5,3 +5,6 @@ export const shopAddress = new PublicKey('J1NkFutMhGVb2sY1gWyHVogWzt76FutUWA7shP
 
 // USDC token address
 export const usdcAddress = new PublicKey('Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr')
+
+// token/coupon address
+export const couponAddress = new PublicKey('9TUumNb23Nq4Ljm7ou8g1Hvd3KSDdEdCqoYzzQfvR7aG')
